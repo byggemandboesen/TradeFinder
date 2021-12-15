afb22ee52e5bbaddb6bc19dd8cb23efe738f1e2d
byggemandboesen <victorboesen@hotmail.com>
2021-12-16 00:52:24 +0100

    :twisted_rightwards_arrows: Migrated to slash commands and pycord

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Discord imports
-# TODO Migrate to pycord
 import discord
-from discord.ext import commands, tasks
+from discord.ext import tasks
 
 #Misc imports
 from datetime import datetime
+import pandas as pd
+import json
 import os
 
 # File imports
@@ -21,27 +22,44 @@ AlertLogger = Logger()
 
 #-------------------------------------- Bot initiation and loop --------------------------------------#
 
+bot = discord.Bot()
 
-# Sets the command prefix for calling the bot
-bot = commands.Bot(command_prefix = ".")
+# Define channels
+with open("parameters.json", "r") as file:
+    parameters = json.load(file)
+    guild_id = parameters["guild_id"]
+    bot_updates_channel = parameters["bot_updates_channel"]
+    
+    alerts_channel = parameters["alerts_channel"]
+    alert_channel = bot.get_channel(alerts_channel)
+    vol_breakout_alerts = parameters["volume_breakout_channel"]
 
 @bot.event
 async def on_ready():
     print("Bot is ready to scrape coins!!")
+    # TODO Fix RuntimeError
     scrape_market.start()
 
 @tasks.loop(seconds = 300)
 async def scrape_market():
-    print(f"{datetime.utcnow()} - Scraping market...")
+    update_channel = bot.get_channel(bot_updates_channel)
+    # await update_channel.send(f"{datetime.utcnow()} - Busy scraping market!")
+    
     # Check for high volume moves to the upside
-    # Retuns data with coins , % up from open price, and the volume
     movers = TradeScraper.check_vol()
+    if movers is None:
+        print("No high volume breakouts!")
+    else:
+        channel = bot.get_channel(vol_breakout_alerts)
+        await channel.send(f"Caught high volume breakouts on3m candles!\n{movers.to_string()}")
+
 
     # Check if any alerts have been triggered
     symbols = list(AlertLogger.get_symbols())
     df = DataStreamer.getKlines(symbols)
     # alerts_triggered = TradeScraper.check_alerts(df)
-    print(f"{datetime.utcnow()} - Done scraping!")
+    # update_channel = bot.get_channel(bot_updates_channel)
+    # await update_channel.send(f"{datetime.utcnow()} - Done scraping!")
 
 @scrape_market.before_loop
 async def set_status():
@@ -53,22 +71,18 @@ async def set_status():
 
 
 # Add an alert for a symbol pair when certain price is crossed
-@bot.command(name = "alert", description = "Add an alert for a symbol pair")
-async def alert(ctx, symbol='', type='', price=''):
+@bot.slash_command(guild_ids = [guild_id], name = "alert", description = "Add an alert for a symbol pair")
+async def alert(ctx, symbol, type, price):
     symbol = symbol.upper()
     # Get user ID
     uid = ctx.message.author.id
-    
-    if '' in (symbol, type, price):
-        await ctx.send("Please remember to specify symbol, type(up/down) and price")
-    else:
-        AlertLogger.add_alert(uid, symbol, type, price)
-        await ctx.send(f"Set an alert for {symbol}!")
+    AlertLogger.add_alert(uid, symbol, type, price)
+    await ctx.send(f"Set an alert for {symbol}!")
 
 
 # Remove alert
-@bot.command(name = "rmalert")
-async def rmalert(ctx, symbol='', type='', price=''):
+@bot.slash_command(guild_ids = [guild_id], name = "rmalert", description = "Remove an alert")
+async def rmalert(ctx, symbol, type, price):
     symbol = symbol.upper()
     uid = ctx.message.author.id
     AlertLogger.rm_alert(uid, symbol, type, price)
@@ -76,12 +90,10 @@ async def rmalert(ctx, symbol='', type='', price=''):
 
 
 # Chart symbol pair last 24H
-@bot.command(name = "chart", description = "Plot a currency pair 24H back")
-async def chart(ctx, symbol=''):
-    symbol = symbol.upper()    
-    if symbol == '':
-        await ctx.send("Oooops, you forgot a symbol pair (BTCUSDT, ETHBTC ...):wink:")
-    elif not DataStreamer.check_symbol(symbol):
+@bot.slash_command(guild_ids = [guild_id], name = "chart", description = "Plot a currency pair 24H back")
+async def chart(ctx, symbol):
+    symbol = symbol.upper()
+    if not DataStreamer.check_symbol(symbol):
         await ctx.send("That sucks.... I looked everywhere on Binance, but I can't find this symbol pair:cry:")
     else:
         candles = DataStreamer.getKlines(symbol, 288)
@@ -91,19 +103,16 @@ async def chart(ctx, symbol=''):
         # Send chart in given path and delete after done
         with open(chart_path, 'rb') as image:
             img = discord.File(image)
-            await ctx.send(file = img)
+            await ctx.respond(file = img)
 
         os.remove(chart_path)
 
 
 # Get the value of a certain indicator from at certain symbol pair
-@bot.command(name = "indicator", description = "Get the latest value of a certain indicator on a certain timeframe")
-async def indicator(ctx, symbol, indicator = '0', timeframe = '1h', window = '0'):
-    available_indicators = TechnicalAnalyzer.available_indicators()
+@bot.slash_command(guild_ids = [guild_id], name = "indicator", description = "Get the latest value of a certain indicator on a certain timeframe")
+async def indicator(ctx, symbol, indicator, window='0', timeframe = '1h'):
     symbol = symbol.upper()
-    if indicator == '0':
-        await ctx.send(f"Whooops, not too fast! You forgot an indicator, {available_indicators}:wink:")
-    elif not DataStreamer.check_symbol(symbol):
+    if not DataStreamer.check_symbol(symbol):
         await ctx.send("That sucks.... I looked everywhere on Binance, but I can't find this symbol pair:cry:")
     elif not DataStreamer.check_timeframe(timeframe):
         await ctx.send("Whooops, that looks like an incorrect timeframe:alarm_clock: Please check your command!")
@@ -115,10 +124,9 @@ async def indicator(ctx, symbol, indicator = '0', timeframe = '1h', window = '0'
         await ctx.send(f"Indicator, \"{indicator}\", for symbol, {symbol}, is currently at:\n{val}\n- on {timeframe} timeframe.")
 
 
-
 # Parse trendline information
 # TODO Figure out a way to parse trendline information
-# @bot.command(name = "parse", description = "Parse trendline information to add to an alert")
+# @bot.slash_command(guild_ids = [guild_id], name = "parse", description = "Parse trendline information to add to an alert")
 # async def parse(ctx, price1, bar1, price2, bar2, timeframe):
 #     point1 = [price1, bar1]
 #     point2 = [price2, bar2]
--- trade_finder.py
+++ trade_finder.py
@@ -3,30 +3,30 @@ import pandas as pd
 class TradeFinder:
     def __init__(self, DataStreamer, TechnicalAnalyzer):
         self.DataStreamer = DataStreamer
-        self.all_coins = [coin for coin in self.DataStreamer.get_symbols() if "USD" in coin and not "USDC" in coin] # ["BTCUSDT", "ETHUSDT", "ADAUSDT", "BNBUSDT", "SOLUSDT"]
+        self.all_coins = [coin for coin in self.DataStreamer.get_symbols() if "USDT" in coin] # ["ETHUSDT", "BTCUSDT", "BNBUSDT", "SOLUSDT", "ADAUSDT"]
         self.TechnicalAnalyzer = TechnicalAnalyzer
-        self.VOL_THREASHOLD = 3
+        self.VOL_THREASHOLD = 4
         
     def check_vol(self):
         # Empty dataframe that contains all coins with a volume greater than 3X the volume moving average
-        columns = ["Coins", "Volume", "Volume avg", "Percent"]
+        columns = ["Coins", "Percent volume increase", "Percent price increase"]
         df = pd.DataFrame()
         for coin in self.all_coins:
             try:
-                candles = self.DataStreamer.getKlines(coin, 20)
-                open, close = float(candles["Open"].iloc[-1]), float(candles["Close"].iloc[-1])
-                # If current price is lower than candle open price don't bother going further
+                candles = self.DataStreamer.getKlines(coin, 21, "3m")
+                open, close = float(candles["Open"].iloc[-2]), float(candles["Close"].iloc[-2])
                 
+                # If current price is lower than candle open price don't bother going further
                 if open > close:
                     continue
                 else:
                     vma = self.TechnicalAnalyzer.vma(candles, 20)
                     percent_up = round((close - open) * 100 / open, 2)
-                    temp_df = pd.DataFrame([[coin, candles["Volume"].iloc[-1], vma, percent_up]], columns = columns)
-                    df = pd.concat([df, temp_df], ignore_index = True) if (float(temp_df["Volume"]) > self.VOL_THREASHOLD * float(temp_df["Volume avg"])) else df
+                    vol_diff = round((candles["Volume"].iloc[-2] - vma) * 100 / vma, 2)
+                    temp_df = pd.DataFrame([[coin, vol_diff, percent_up]], columns = columns)
+                    df = pd.concat([df, temp_df], ignore_index = True) if (float(temp_df["Percent volume increase"]) > self.VOL_THREASHOLD * 100) else df
             except Exception as e:
                 print(f"Skipping coin due to exception: \"{e}\"")
         
-        df = df if len(df.index) == 0 else df.set_index("Coins", inplace=True, drop=True)
-        print(df)
+        df = df.set_index("Coins", inplace = False) if len(df.index) > 0 else None
         return df
